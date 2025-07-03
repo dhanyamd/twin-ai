2c830eb189098220ae8d89d4ec7e354aa1fdf1b8
Dhanya MD <dhanya13md@gmail.com>
2025-07-03 17:01:27 +0530

    add docker

---
+++ src/embedding_pipleline/pipeline.py
@@ -0,0 +1,12 @@
+from vector_twin.settings import settings
+from steps import generate_embeddings, load_hf_dataset, sample_dataset
+from zenml import pipeline
+from zenml.config import DockerSettings
+from zenml.config.docker_settings import DockerBuildConfig 
+
+docker_settings = DockerSettings(build_config=DockerBuildConfig(dockerignore=".dockerignore"))
+@pipeline(settings={"docker": docker_settings})
+def embedding_pipeline(use_qdrant_cloud: bool):
+    dataset = load_hf_dataset()
+    sampled_dataset = sample_dataset(dataset)
+    generate_embeddings(sampled_dataset, use_qdrant_cloud)
